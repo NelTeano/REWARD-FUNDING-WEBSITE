cbb2967eab913896a74e99574f60a468d78fdd07
JONEL <jonelteano29@gmail.com>
2024-05-13 23:00:58 +0800

    make the sign up/in login page

--- src/app/sign-up/[[...sign-up]]/page.js
+++ src/app/sign-up/[[...sign-up]]/page.js
@@ -1,5 +1,72 @@
 import { SignUp } from "@clerk/nextjs";
+import {
+    Container, 
+    Box
+} from '@mui/material';
+
+import Image from "next/image";
+
+// IMAGES
+import RewFundBanner from '../../../../public/assets/RewFundLoginBanner.png'
 
 export default function Page() {
-    return <SignUp path="/sign-up" />;
+    return (
+        <>
+            <Container 
+                disableGutters={true} 
+                maxWidth='100%' 
+                sx={{
+                        height: '100%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        flexDirection: 'column',
+                        backgroundColor: '#0F111D',
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        width: '80%',
+                        height: '800px',
+                        backgroundColor: '#0F111D',
+                        mt: '5rem'
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            width: '100%',
+                            height: '800px',
+                            backgroundColor: 'white'
+                        }}
+                    >
+                        <Image 
+                            src={RewFundBanner}
+                            priority={true}
+                            width="0"
+                            height="0"
+                            className='h-full w-full relative'
+                            alt='product image'
+                        />
+                    </Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            width: '100%',
+                            height: '800px',
+                            backgroundColor: 'white'
+                        }}
+                    >
+                        <SignUp path="/sign-up" />
+                    </Box>      
+                </Box>
+            </Container>
+        </>
+        
+
+    );
 }
